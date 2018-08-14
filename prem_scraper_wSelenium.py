232f3e68fdffe38f22f32738580e1f55c341e4a4
julianashwin <julianashwin@gmail.com>
2018-08-14 14:30:16 +0100

    dataframe
    Touches and name now written into a dataframe

--- prem_scraper_wSelenium.py
+++ prem_scraper_wSelenium.py
@@ -11,6 +11,8 @@ from selenium.webdriver.support.ui import Select
 
 import requests
 import pandas as pd
+import numpy as np
+import time
 from bs4 import BeautifulSoup
 
 driver = webdriver.Chrome(path_to_chromedriver)
@@ -18,19 +20,20 @@ driver = webdriver.Chrome(path_to_chromedriver)
 driver.get("https://www.premierleague.com/stats/top/players/touches")
 
 html = driver.page_source
+soup = BeautifulSoup(html, "html.parser")
 
-soup = BeautifulSoup(html)
-
-
-
-url_pagination = "https://www.premierleague.com/stats/top/players/touches"
-r = requests.get(url_pagination)
-soup = BeautifulSoup(r.content, "html.parser")
+# Create an empty dataframe to fill iteratively
+columns = ['player_name','touches', 'year']
+touches_df = pd.DataFrame(columns = columns)
 
 for i in range(1,100):
+
+    time.sleep(np.random.uniform(1.5,3))
+    #wait = WebDriverWait(driver, 300)
+    #ok_button_success = wait.until(EC.element_to_be_clickable((By.css_selector, "div.paginationBtn.paginationNextContainer")))
     print "Scraping page %d" % i
 
-    temp_df = pd.DataFrame()
+    temp_df = pd.DataFrame(index = range(0,20), columns = columns)
 
     title =  soup.title.text
     print(title)
@@ -38,16 +41,37 @@ for i in range(1,100):
     print(name)
 
     player_names = soup.find_all('a', {'class': 'playerName'})
-
-    player_names_vec = np.chararray((20,1))
-
-    for (a,b) in player_names:
-        player_names_vec[[]] = a.text
-
     main_stat = soup.find_all('td', {'class': 'mainStat'})
-    for a in main_stat:
-        print(a.text)
+
+    player_names_vec = []
+    touches_vec = []
+
+    for a in range(0,20):
+        temptext = player_names[a].text
+        temptext = temptext.encode("utf-8")
+        print temptext
+        player_names_vec.append(temptext)
+
+        temptext = main_stat[a].text
+        temptext = temptext.encode("utf-8")
+        touches_vec.append(temptext)
+
+    temp_df.player_name = player_names_vec
+    temp_df.touches = touches_vec
+
+    touches_df = touches_df.append(temp_df)
 
     next_button = driver.find_element_by_css_selector("div.paginationBtn.paginationNextContainer")
 
     next_button.click()
+
+    html = driver.page_source
+    soup = BeautifulSoup(html, "html.parser")
+
+
+touches_df.touches = touches_df.touches.str.replace(",","")
+touches_df.touches = touches_df.touches.astype(int)
+
+# Plot histogram
+plt.hist(touches_df.touches)
+plt.show()
