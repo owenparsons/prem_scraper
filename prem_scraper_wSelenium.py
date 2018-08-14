d1a6bb5c7a4834b1fed0844ea069ea68d75c76a6
julianashwin <julianashwin@gmail.com>
2018-08-14 15:06:51 +0100

    cycle through each year

--- prem_scraper_wSelenium.py
+++ prem_scraper_wSelenium.py
@@ -25,20 +25,17 @@ soup = BeautifulSoup(html, "html.parser")
 # Create an empty dataframe to fill iteratively
 columns = ['player_name','touches', 'year']
 touches_df = pd.DataFrame(columns = columns)
+prev_names = []
 
+year = "2015/2016"
 for i in range(1,100):
 
     time.sleep(np.random.uniform(1.5,3))
     #wait = WebDriverWait(driver, 300)
     #ok_button_success = wait.until(EC.element_to_be_clickable((By.css_selector, "div.paginationBtn.paginationNextContainer")))
     print "Scraping page %d" % i
-
-    temp_df = pd.DataFrame(index = range(0,20), columns = columns)
-
-    title =  soup.title.text
-    print(title)
-    name = soup.find('span', {'class': 'badge-50 LEI '}).text
-    print(name)
+    html = driver.page_source
+    soup = BeautifulSoup(html, "html.parser")
 
     player_names = soup.find_all('a', {'class': 'playerName'})
     main_stat = soup.find_all('td', {'class': 'mainStat'})
@@ -46,7 +43,9 @@ for i in range(1,100):
     player_names_vec = []
     touches_vec = []
 
-    for a in range(0,20):
+    temp_df = pd.DataFrame(index = range(0,len(player_names)), columns = columns)
+
+    for a in range(0,len(player_names)):
         temptext = player_names[a].text
         temptext = temptext.encode("utf-8")
         print temptext
@@ -58,15 +57,20 @@ for i in range(1,100):
 
     temp_df.player_name = player_names_vec
     temp_df.touches = touches_vec
+    temp_df.touches = temp_df.touches.str.replace(",","")
+    temp_df.touches = temp_df.touches.astype(int)
+
+    temp_df.year = year
+
+    if prev_names == player_names_vec:
+        break
 
     touches_df = touches_df.append(temp_df)
 
     next_button = driver.find_element_by_css_selector("div.paginationBtn.paginationNextContainer")
-
     next_button.click()
 
-    html = driver.page_source
-    soup = BeautifulSoup(html, "html.parser")
+    prev_names = player_names_vec
 
 
 touches_df.touches = touches_df.touches.str.replace(",","")
